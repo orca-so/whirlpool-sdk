38473a7ff3f576876fdaa5622ff55c890360096f
scuba <88916581+0xscuba@users.noreply.github.com>
2021-12-06 22:02:46 -0800

    Cache manager (#4)
    * Cache manager
    
    * minor update

---
+++ src/model/cache/orca-cache.ts
@@ -0,0 +1,113 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+import { OrcaCacheInternal, OrcaCache } from "../..";
+import {
+  getWhirlpoolProgramId,
+  getWhirlpoolsConfig,
+  Network,
+  OrcaCacheContentType,
+  OrcaCacheKey,
+  Position,
+  TickArray,
+  Whirlpool,
+  OrcaCacheContentValue,
+} from "../../public";
+
+export class OrcaCacheImpl implements OrcaCache {
+  public readonly whirlpoolsConfig: PublicKey;
+  public readonly programId: PublicKey;
+
+  private _connection: Connection;
+  private _cache: OrcaCacheInternal = {};
+
+  constructor(network: Network, connection: Connection) {
+    this.whirlpoolsConfig = getWhirlpoolsConfig(network);
+    this.programId = getWhirlpoolProgramId(network);
+    this._connection = connection;
+  }
+
+  public async getWhirlpool(address: PublicKey | string): Promise<Whirlpool> {
+    return this.get(address, OrcaCacheContentType.Whirlpool);
+  }
+
+  public async getPosition(address: PublicKey | string): Promise<Position> {
+    return this.get(address, OrcaCacheContentType.Position);
+  }
+
+  public async getTickArray(address: PublicKey | string): Promise<TickArray> {
+    return this.get(address, OrcaCacheContentType.TickArray);
+  }
+
+  private async get(
+    address: PublicKey | string,
+    type: OrcaCacheContentType
+  ): Promise<OrcaCacheContentValue> {
+    const cachedValue = this.getCached(address);
+    if (cachedValue) {
+      return cachedValue;
+    }
+
+    return this.add(address, type);
+  }
+
+  public getCached(address: PublicKey | string): OrcaCacheContentValue | null {
+    const key = OrcaCacheImpl.toAddressString(address);
+    return this._cache[key]?.value || null;
+  }
+
+  public getCachedAll(): [OrcaCacheKey, OrcaCacheContentValue][] {
+    return Object.entries(this._cache).map(([key, content]) => [key, content.value]);
+  }
+
+  public async add(
+    address: PublicKey | string,
+    type: OrcaCacheContentType
+  ): Promise<OrcaCacheContentValue> {
+    const pk = OrcaCacheImpl.toPublicKey(address);
+
+    let fetch: (() => Promise<OrcaCacheContentValue>) | null = null;
+
+    if (type === OrcaCacheContentType.Whirlpool) {
+      fetch = () => Whirlpool.fetch(pk);
+    } else if (type === OrcaCacheContentType.Position) {
+      fetch = () => Position.fetch(pk);
+    } else if (type === OrcaCacheContentType.TickArray) {
+      fetch = () => TickArray.fetch(pk);
+    } else {
+      throw new Error(`${type} is not a known OrcaAccountType`);
+    }
+
+    const key = OrcaCacheImpl.toAddressString(address);
+
+    const value = await fetch();
+    this._cache[key] = { type, value, fetch };
+    return value;
+  }
+
+  public async refresh(address: PublicKey | string): Promise<void> {
+    const key = OrcaCacheImpl.toAddressString(address);
+
+    const cachedContent = this._cache[key];
+    if (!cachedContent) {
+      throw new Error("unable to refresh non-existing key");
+    }
+
+    const value = await cachedContent.fetch();
+    this._cache[key] = { ...cachedContent, value };
+  }
+
+  // TODO - use batch account fetch, then parse individually, instead of individual fetch and parse
+  public async refreshAll(): Promise<void> {
+    for (const [address, cachedContent] of Object.entries(this._cache)) {
+      const value = await cachedContent.fetch();
+      this._cache[address] = { ...cachedContent, value };
+    }
+  }
+
+  private static toAddressString(address: PublicKey | string): string {
+    return typeof address === "string" ? address : address.toBase58();
+  }
+
+  private static toPublicKey(address: PublicKey | string): PublicKey {
+    return typeof address === "string" ? new PublicKey(address) : address;
+  }
+}
---
+++ src/public/cache/types.ts
@@ -0,0 +1,66 @@
+import { PublicKey } from "@solana/web3.js";
+import { Position, TickArray, Whirlpool } from "..";
+
+export enum OrcaCacheContentType {
+  Whirlpool = "WHIRLPOOL",
+  Position = "POSITION",
+  TickArray = "TICK_ARRAY",
+}
+
+export type OrcaCacheContentValue = Whirlpool | Position | TickArray;
+
+interface OrcaCacheContent {
+  type: OrcaCacheContentType;
+  value: OrcaCacheContentValue;
+  fetch: () => Promise<OrcaCacheContentValue>;
+}
+
+export type OrcaCacheKey = string;
+
+export type OrcaCacheInternal = Record<OrcaCacheKey, OrcaCacheContent>;
+
+export interface OrcaCache {
+  /**
+   * Get a Whirlpool entity from the cache.
+   * If it doesn't exist in the cache, then fetch, save to cache, then return.
+   */
+  getWhirlpool: (address: PublicKey | string) => Promise<Whirlpool>;
+
+  /**
+   * Get a Position entity from the cache.
+   * If it doesn't exist in the cache, then fetch, save to cache, then return.
+   */
+  getPosition: (address: PublicKey | string) => Promise<Position>;
+
+  /**
+   * Get a TickArray entity from the cache.
+   * If it doesn't exist in the cache, then fetch, save to cache, then return.
+   */
+  getTickArray: (address: PublicKey | string) => Promise<TickArray>;
+
+  /**
+   * Check if an entity with the given address exists.
+   * Return the entity if it exists, throw an error if it does not.
+   */
+  getCached: (address: PublicKey | string) => OrcaCacheContentValue | null;
+
+  /**
+   * Return entries of all the key, value pairs in the cache
+   */
+  getCachedAll: () => [OrcaCacheKey, OrcaCacheContentValue][];
+
+  /**
+   * Manually add an entity with the given address and type to the cache.
+   */
+  add: (address: PublicKey | string, type: OrcaCacheContentType) => Promise<OrcaCacheContentValue>;
+
+  /**
+   * Update the cache of the entity with the given address.
+   */
+  refresh: (address: PublicKey | string) => Promise<void>;
+
+  /**
+   * Update the cached value of all entities currently in the cache.
+   */
+  refreshAll: () => Promise<void>;
+}
