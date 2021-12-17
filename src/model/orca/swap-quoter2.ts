c9275659c197ad38a685b6495692575ae1f2655b
atamari <atul@orca.so>
2021-12-17 15:36:10 +0400

    Make progress on swap quote cleanup

--- src/model/orca/swap-quoter2.ts
+++ src/model/orca/swap-quoter2.ts
@@ -86,7 +86,7 @@ class SwapSimulator<A extends Token, B extends Token> {
   public simulateSwapStep(input: SwapStepSimulationInput): SwapStepSimulationOutput {
     const { slippageTolerance, swapDirection, amountSpecified, feeRate } = this.config;
     const { calculateTargetSqrtPrice } = SwapSimulator.functionsBySwapDirection[swapDirection];
-    const { calculateSpecifiedTokenDelta } =
+    const { calculateSpecifiedTokenDelta, calculateNextSqrtPriceGivenTokenDelta } =
       SwapSimulator.functionsBySwapType[swapDirection][amountSpecified];
     const {
       amount: specifiedTokenAmount,
@@ -111,16 +111,28 @@ class SwapSimulator<A extends Token, B extends Token> {
       targetSqrtPriceX64
     );
 
-    const specifiedTokenAmountMaxDelta = calculateSpecifiedTokenDelta(
+    const specifiedTokenMaxDelta = calculateSpecifiedTokenDelta(
       currentLiquidity,
       sqrtPriceLowerX64,
       sqrtPriceUpperX64
     );
 
-    const specifiedTokenAmountAfterFees = SwapSimulator.calculateAmountAfterFees(
+    const specifiedTokenDelta = SwapSimulator.calculateAmountAfterFees(
       specifiedTokenAmount,
       feeRate
     );
+
+    let nextSqrtPriceX64: BN;
+    if (specifiedTokenDelta.gte(specifiedTokenMaxDelta)) {
+      // Fully utilize this tick range
+      nextSqrtPriceX64 = targetSqrtPriceX64;
+    } else {
+      nextSqrtPriceX64 = calculateNextSqrtPriceGivenTokenDelta(
+        specifiedTokenDelta,
+        currentLiquidity,
+        currentSqrtPriceX64
+      );
+    }
 
     TODO("Complete");
   }
@@ -167,38 +179,117 @@ class SwapSimulator<A extends Token, B extends Token> {
         : BNUtils.x64ToX0Ceil(tokenBDeltaX64);
     };
 
+  private static calculateSqrtPriceAtPrevInitializedTick(
+    currentTickArrayAccount: TickArrayAccount,
+    currentTickIndex: number,
+    currentSqrtPriceX64: BN,
+    sqrtPriceSlippageX64: BN
+  ): BN {
+    return BN.max(
+      currentSqrtPriceX64.sub(sqrtPriceSlippageX64),
+      TickMath.sqrtPriceAtTick(
+        TickArray.getPrevInitializedTickIndex(currentTickArrayAccount, currentTickIndex)
+      )
+    );
+  }
+
+  private static calculateSqrtPriceAtNextInitializedTick(
+    currentTickArrayAccount: TickArrayAccount,
+    currentTickIndex: number,
+    currentSqrtPriceX64: BN,
+    sqrtPriceSlippageX64: BN
+  ): BN {
+    return BN.min(
+      currentSqrtPriceX64.add(sqrtPriceSlippageX64),
+      TickMath.sqrtPriceAtTick(
+        TickArray.getNextInitializedTickIndex(currentTickArrayAccount, currentTickIndex)
+      )
+    );
+  }
+
+  // TODO: Account for rounding
+  private static calculateLowerSqrtPriceGivenTokenADelta(
+    remainingTokenAAmountX0: BN,
+    currentLiquidityX0: BN,
+    currentSqrtPriceX64: BN
+  ): BN {
+    // To compute this, we use eq 6.15 from univ3 whitepaper:
+    // Δ(1/√P) = ΔX/L => 1/sqrt(lower) - 1/sqrt(upper) = tokenAToSwap/state.currLiquidity
+    // What we're trying to find here is actually sqrt(lower) , so let's solve for that:
+    //  => 1/sqrt(lower) = tokenAToSwap/state.currLiquidity + 1/sqrt(upper)
+    //  => 1/sqrt(lower) = (tokenAToSwap*sqrt(upper) + state.currLiquidity) / (state.currLiquidity * sqrt(upper))
+    //  => sqrt(lower) = (state.currLiquidity * sqrt(upper)) / (tokenAToSwap*sqrt(upper) + state.currLiquidity)
+    // Precision analysis: (u64 * q64.64) / (u64 * q64.64 + u64)
+    const numeratorX64 = currentLiquidityX0.mul(currentSqrtPriceX64);
+    const denominatorX64 = remainingTokenAAmountX0
+      .mul(currentSqrtPriceX64)
+      .add(BNUtils.x0ToX64(currentLiquidityX0));
+
+    return BNUtils.divX64(numeratorX64, denominatorX64);
+  }
+
+  // TODO: Account for rounding
+  private static calculateUpperSqrtPriceGivenTokenADelta(
+    remainingTokenAAmountX0: BN,
+    currentLiquidityX0: BN,
+    currentSqrtPriceX64: BN
+  ): BN {
+    // To compute this, we use eq 6.15 from univ3 whitepaper:
+    // Δ(1/√P) = ΔX/L => 1/sqrt(lower) - 1/sqrt(upper) = remainingAToWithdraw/state.currLiquidity
+    // What we're trying to find here is actually sqrt(upper) , so let's solve for that:
+    //  => 1/sqrt(upper) =  1/sqrt(lower) - remainingAToWithdraw/state.currLiquidity
+    //  => sqrt(upper) =  (state.currLiquidity * sqrt(lower)) / (state.currLiquidity - sqrt(lower)*remainingAToWithdraw)
+    // Precision analysis raw: (u64 * q64.64) / (u64 - q64.64 * u64)
+    const numeratorX64 = currentLiquidityX0.mul(currentSqrtPriceX64);
+    const denominatorX64 = BNUtils.x0ToX64(currentLiquidityX0).sub(
+      currentSqrtPriceX64.mul(remainingTokenAAmountX0)
+    );
+
+    return BNUtils.divX64(numeratorX64, denominatorX64);
+  }
+
+  // TODO: Account for rounding
+  private static calculateUpperSqrtPriceGivenTokenBDelta(
+    remainingTokenBAmountX0: BN,
+    currentLiquidityX0: BN,
+    currentSqrtPriceX64: BN
+  ): BN {
+    // To compute this, we use eq 6.13 from univ3 whitepaper:
+    // Δ√P = ΔY/L => sqrt(upper) - sqrt(lower) = remainingBToSwap / state.currLiquidity
+    // What we're trying to find here is actually sqrt(upper) , so let's solve for that:
+    //  => sqrt(upper) = (remainingBToSwap / state.currLiquidity) + sqrt(lower)
+    // Precision analysis: (q64.0 / q64.0) + q64.64
+    return BNUtils.x0ToX64(remainingTokenBAmountX0)
+      .div(currentLiquidityX0)
+      .add(currentSqrtPriceX64);
+  }
+
+  // TODO: Account for rounding
+  private static calculateLowerSqrtPriceGivenTokenBDelta(
+    remainingTokenBAmountX0: BN,
+    currentLiquidityX0: BN,
+    currentSqrtPriceX64: BN
+  ): BN {
+    // To compute this, we use eq 6.13 from univ3 whitepaper:
+    // Δ√P = ΔY/L => sqrt(upper) - sqrt(lower) = remainingBToWithdraw / state.currLiquidity
+    // What we're trying to find here is actually sqrt(lower) since we're removing B from the pool, so let's solve for that:
+    //  => sqrt(lower) = sqrt(upper) - (remainingBToWitdhraw / state.currLiquidity)
+    // Precision analysis: q64.64 - (q64.0 / q64.0)
+    return currentSqrtPriceX64.sub(
+      BNUtils.x0ToX64(remainingTokenBAmountX0).div(currentLiquidityX0)
+    );
+  }
+
   private static readonly functionsBySwapDirection = {
     [SwapDirection.AtoB]: {
       // TODO: Account for edge case where we're at MIN_TICK
       // TODO: Account for moving between tick arrays (support one adjacent tick array to the left)
-      calculateTargetSqrtPrice: (
-        currentTickArrayAccount: TickArrayAccount,
-        currentTickIndex: number,
-        currentSqrtPriceX64: BN,
-        sqrtPriceSlippageX64: BN
-      ) =>
-        BN.max(
-          currentSqrtPriceX64.sub(sqrtPriceSlippageX64),
-          TickMath.sqrtPriceAtTick(
-            TickArray.getPrevInitializedTickIndex(currentTickArrayAccount, currentTickIndex)
-          )
-        ),
+      calculateTargetSqrtPrice: SwapSimulator.calculateSqrtPriceAtPrevInitializedTick,
     },
     [SwapDirection.BtoA]: {
       // TODO: Account for edge case where we're at MAX_TICK
       // TODO: Account for moving between tick arrays (support one adjacent tick array to the right)
-      calculateTargetSqrtPrice: (
-        currentTickArrayAccount: TickArrayAccount,
-        currentTickIndex: number,
-        currentSqrtPriceX64: BN,
-        sqrtPriceSlippageX64: BN
-      ) =>
-        BN.min(
-          currentSqrtPriceX64.add(sqrtPriceSlippageX64),
-          TickMath.sqrtPriceAtTick(
-            TickArray.getNextInitializedTickIndex(currentTickArrayAccount, currentTickIndex)
-          )
-        ),
+      calculateTargetSqrtPrice: SwapSimulator.calculateSqrtPriceAtNextInitializedTick,
     },
   };
 
@@ -206,17 +297,25 @@ class SwapSimulator<A extends Token, B extends Token> {
     [SwapDirection.AtoB]: {
       [AmountSpecified.Input]: {
         calculateSpecifiedTokenDelta: SwapSimulator.calculateTokenADelta(Rounding.Up),
+        calculateNextSqrtPriceGivenTokenDelta:
+          SwapSimulator.calculateLowerSqrtPriceGivenTokenADelta,
       },
       [AmountSpecified.Output]: {
         calculateSpecifiedTokenDelta: SwapSimulator.calculateTokenBDelta(Rounding.Down),
+        calculateNextSqrtPriceGivenTokenDelta:
+          SwapSimulator.calculateLowerSqrtPriceGivenTokenBDelta,
       },
     },
     [SwapDirection.BtoA]: {
       [AmountSpecified.Input]: {
         calculateSpecifiedTokenDelta: SwapSimulator.calculateTokenBDelta(Rounding.Up),
+        calculateNextSqrtPriceGivenTokenDelta:
+          SwapSimulator.calculateUpperSqrtPriceGivenTokenBDelta,
       },
       [AmountSpecified.Output]: {
         calculateSpecifiedTokenDelta: SwapSimulator.calculateTokenADelta(Rounding.Down),
+        calculateNextSqrtPriceGivenTokenDelta:
+          SwapSimulator.calculateUpperSqrtPriceGivenTokenADelta,
       },
     },
   };
