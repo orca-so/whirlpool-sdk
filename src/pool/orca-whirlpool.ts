7ec38abbc94a153d39656b682cf2395869b42bf4
atamari <91073941+atamari-orca@users.noreply.github.com>
2022-01-13 07:13:03 +0400

    OrcaWhirlpool class skeleton + getOpenPositionQuote (#32)
    * Init skeleton for OrcaWhirlpool following OrcaPosition
    
    * getOpenPositionQuote done
    
    * Remove unused types

--- src/pool/orca-whirlpool.ts
+++ src/pool/orca-whirlpool.ts
@@ -1,6 +1,137 @@
 // import invariant from "tiny-invariant";
 // import { OrcaDAL } from "../dal/orca-dal";
 
+import { WhirlpoolData } from "@orca-so/whirlpool-client-sdk/dist/types/anchor-types";
+import { MintInfo, u64 } from "@solana/spl-token";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import invariant from "tiny-invariant";
+import {
+  ClosePositionTransactionParam,
+  ClosePositoinTransaction,
+  OpenPositionQuote,
+  OpenPositionQuoteParam,
+  OpenPositionTransaction,
+  OpenPositionTransactionParam,
+  PositionStatus,
+  SwapQuote,
+  SwapQuoteParam,
+  SwapTransaction,
+  SwapTransactionParam,
+} from "..";
+import { defaultSlippagePercentage } from "../constants/defaults";
+import { OrcaDAL } from "../dal/orca-dal";
+import { OrcaPosition } from "../position/orca-position";
+import {
+  getAddLiquidityQuoteWhenPositionIsAboveRange,
+  getAddLiquidityQuoteWhenPositionIsBelowRange,
+  getAddLiquidityQuoteWhenPositionIsInRange,
+  InternalAddLiquidityQuoteParam,
+} from "../position/quotes/add-liquidity";
+
+function TODO(): never {
+  throw new Error("TODO: Implement");
+}
+
+export class OrcaWhirlpool {
+  constructor(private readonly dal: OrcaDAL) {}
+
+  /*** Transactions (public) ***/
+
+  /** 1. Open position tx **/
+  public async getOpenPositionTransaction(
+    param: OpenPositionTransactionParam
+  ): Promise<OpenPositionTransaction> {
+    TODO();
+  }
+
+  /** 2. Close position tx **/
+  public async getClosePositionTransaction(
+    param: ClosePositionTransactionParam
+  ): Promise<ClosePositoinTransaction> {
+    TODO();
+  }
+
+  /** 3. Swap tx **/
+  public async getSwapTransaction(param: SwapTransactionParam): Promise<SwapTransaction> {
+    TODO();
+  }
+
+  /*** Quotes (public) ***/
+
+  /** 1. Open position quote **/
+  public async getOpenPositionQuote(param: OpenPositionQuoteParam): Promise<OpenPositionQuote> {
+    const {
+      whirlpoolAddress,
+      tokenMint,
+      tokenAmount,
+      tickLowerIndex,
+      tickUpperIndex,
+      slippageTolerence = defaultSlippagePercentage,
+      refresh,
+    } = param;
+
+    const dummyPosition = {
+      whirlpool: whirlpoolAddress,
+      positionMint: Keypair.generate().publicKey,
+      liquidity: new u64(0),
+      tickLowerIndex,
+      tickUpperIndex,
+      // TODO(atamari): Make sure these values make sense (or we just treat this is a dummy value and ignore)
+      feeGrowthCheckpointA: new u64(0),
+      feeOwedA: new u64(0),
+      feeGrowthCheckpointB: new u64(0),
+      feeOwedB: new u64(0),
+      rewardInfos: [],
+    };
+
+    const whirlpool = await this.getWhirlpool(whirlpoolAddress, refresh);
+    const [tokenAMintInfo, tokenBMintInfo] = await this.getTokenMintInfos(whirlpool, refresh);
+
+    const addLiquidityParams: InternalAddLiquidityQuoteParam = {
+      whirlpool,
+      position: dummyPosition,
+      tokenAMintInfo,
+      tokenBMintInfo,
+      tokenMint,
+      tokenAmount,
+      slippageTolerence,
+    };
+
+    switch (OrcaPosition.getPositionStatus(whirlpool, dummyPosition)) {
+      case PositionStatus.BelowRange:
+        return getAddLiquidityQuoteWhenPositionIsBelowRange(addLiquidityParams);
+      case PositionStatus.InRange:
+        return getAddLiquidityQuoteWhenPositionIsInRange(addLiquidityParams);
+      case PositionStatus.AboveRange:
+        return getAddLiquidityQuoteWhenPositionIsAboveRange(addLiquidityParams);
+    }
+  }
+
+  /** 2. Swap quote **/
+  public async getSwapQuote(param: SwapQuoteParam): Promise<SwapQuote> {
+    TODO();
+  }
+
+  /*** Helpers (private) ***/
+  private async getWhirlpool(address: PublicKey, refresh = false): Promise<WhirlpoolData> {
+    const whirlpool = await this.dal.getPool(address, refresh);
+    invariant(!!whirlpool, "OrcaWhirlpool - whirlpool does not exist");
+    return whirlpool;
+  }
+
+  private async getTokenMintInfos(
+    whirlpool: WhirlpoolData,
+    refresh = false
+  ): Promise<[MintInfo, MintInfo]> {
+    const mintInfos = await this.dal.listMintInfos(
+      [whirlpool.tokenMintA, whirlpool.tokenMintB],
+      refresh
+    );
+    invariant(!!mintInfos && mintInfos.length === 2, "OrcaWhirlpool - mint infos do not exist");
+    return [mintInfos[0], mintInfos[1]];
+  }
+}
+
 // /**
 //  * Random notes: nft represents the authority to a specific position
 //  */
