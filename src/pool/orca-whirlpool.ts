3ba689cad123a430910617f04162047f2102a19e
0xscuba <88916581+0xscuba@users.noreply.github.com>
2022-01-17 16:15:32 -1000

    cleanup

--- src/pool/orca-whirlpool.ts
+++ src/pool/orca-whirlpool.ts
@@ -1,6 +1,3 @@
-// import invariant from "tiny-invariant";
-// import { OrcaDAL } from "../dal/orca-dal";
-
 import {
   fromX64,
   getPositionPda,
@@ -43,7 +40,6 @@ import {
   getAddLiquidityQuoteWhenPositionIsInRange,
   InternalAddLiquidityQuoteParam,
 } from "../position/quotes/add-liquidity";
-import { InternalRemoveLiquidityQuoteParam } from "../position/quotes/remove-liquidity";
 import { DecimalUtil } from "../utils/decimal-utils";
 import { TransactionExecutable } from "../utils/public/transaction-executable";
 import { resolveOrCreateAssociatedTokenAddress } from "../utils/web3/ata-utils";
@@ -51,10 +47,6 @@ import { PoolUtil } from "../utils/whirlpool/pool-util";
 import { PositionStatus, PositionUtil } from "../utils/whirlpool/position-util";
 import { TickArrayOutOfBoundsError, TickUtil } from "../utils/whirlpool/tick-util";
 import { AmountSpecified, SwapDirection, SwapSimulator } from "./quotes/swap-quoter";
-
-function TODO(message?: string): never {
-  throw new Error(`TODO: ${message || "Implement"}`);
-}
 
 export class OrcaWhirlpool {
   constructor(private readonly dal: OrcaDAL) {}
@@ -541,144 +533,3 @@ export class OrcaWhirlpool {
     );
   }
 }
-
-// /**
-//  * Random notes: nft represents the authority to a specific position
-//  */
-// export class OrcaWhirlpool {
-//   private readonly dal: OrcaDAL;
-
-//   constructor(dal: OrcaDAL) {
-//     this.dal = dal;
-//   }
-
-//   // create whirlpool and tickarray accounts
-//   public async getInitPoolTransaction(
-//     initialPrice: TokenPrice<A, B> | TokenPrice<B, A>
-//   ): Promise<any> {
-//     // TODO(atamari): Confirm that token A is base and token B is quote always
-
-//     // from yutaro feedback:
-//     // 1. Token A should always be the base and token B should always be the quote.
-//     // 2. Token A should always be the base and token B should always be the quote.
-//     // SCUBA-ATAMARI: we should add the token sort logic here as well
-
-//     const normalizedInitialPrice = initialPrice.matchBaseAndQuote(this.tokenA, this.tokenB);
-
-//     // TODO: compute the initial sqrt price from initial price
-//     // TODO: get all accounts (pubkeys) needed to init this pool
-//     // TODO: build the init pool ix
-
-//     // TODO: compute initial tick array params
-//     // TODO: get all accounts (pubkeys) needed to init the tick array
-//     // TODO: build the init tick array ix
-
-//     // TODO: Return one tx to init pool + init tick array
-
-//     throw new Error("TODO - implement");
-//   }
-
-//   public async getOpenPositionQuote(
-//     token: A | B,
-//     tokenAmount: u64,
-//     tickLowerIndex: number,
-//     tickUpperIndex: number,
-//     slippageTolerence = defaultSlippagePercentage
-//   ): Promise<{ maxTokenA: u64; maxTokenB: u64; liquidity: u64 }> {
-//     const { sqrtPriceX64: sqrtPrice } = await this.getWhirlpool();
-
-//     const sqrtPriceLower = TickMath.sqrtPriceAtTick(tickLowerIndex);
-//     const sqrtPriceUpper = TickMath.sqrtPriceAtTick(tickUpperIndex);
-
-//     const tokenAmountX64 = BNUtils.u64ToX64(tokenAmount);
-
-//     // 3.2.1 Example 1: Amount of assets from a range
-//     const LxX64 = tokenAmountX64
-//       .mul(sqrtPrice)
-//       .mul(sqrtPriceUpper)
-//       .div(sqrtPriceUpper.sub(sqrtPrice));
-//     const yX64 = LxX64.mul(sqrtPrice.sub(sqrtPriceLower));
-//     const yU64 = BNUtils.x64ToU64Floor(yX64);
-
-//     throw new Error("TODO - implement");
-//   }
-
-//   public async getOpenPositionTransaction(
-//     owner: Owner,
-//     tokenAccountA: any,
-//     tokenAccountB: any,
-//     token: any,
-//     tokenAmount: any,
-//     tickLowerIndex: number,
-//     tickUpperIndex: number,
-//     slippageTolerence?: Percentage | undefined
-//   ): Promise<any> {}
-
-//   public async getSwapQuote(
-//     swapAmount: SwapAmount<A, B>,
-//     slippageTolerance = defaultSlippagePercentage
-//   ): Promise<SwapQuote<A, B>> {
-//     const whirlpool = await this.getWhirlpool();
-//     const currentTickArray = await this.getCurrentTickArray();
-
-//     return getSwapQuote({
-//       whirlpool,
-//       currentTickArray,
-//       tokenA: this.tokenA,
-//       tokenB: this.tokenB,
-//       amount: swapAmount,
-//       slippageTolerance,
-//     });
-//   }
-
-//   public async getSwapTransaction(
-//     owner: Owner,
-//     tokenAccountA: any,
-//     tokenAccountB: any,
-//     amount: any,
-//     slippageTolerence?: Percentage | undefined
-//   ): Promise<any> {
-//     throw new Error("TODO");
-//   }
-
-//   public async getLiquidityDistribution(): Promise<any> {
-//     throw new Error("TODO");
-//   }
-
-//   public async getSuggestedPriceRange(conservative: boolean): Promise<any> {
-//     throw new Error("TODO");
-//   }
-
-//   public async loadTickArray(tickIndex: number): Promise<TickArrayAccount> {
-//     const whirlpool = await this.getWhirlpool();
-
-//     const tickArrayAddress = TickArrayEntity.getAddressContainingTickIndex(
-//       tickIndex,
-//       whirlpool,
-//       this.dal.programId
-//     );
-//     const tickArray = await this.dal.getTickArray(tickArrayAddress);
-//     invariant(!!tickArray, "loadTickArray - tick_array does not exist");
-
-//     return tickArray;
-//   }
-
-//   private async getWhirlpool(): Promise<WhirlpoolAccount> {
-//     const whirlpool = await this.dal.getWhirlpool(this.address);
-//     invariant(!!whirlpool, "OrcaWhirlpool - whirlpool does not exist");
-//     return whirlpool;
-//   }
-
-//   private async getCurrentTickArray(): Promise<TickArrayAccount> {
-//     const { tickArrayStart } = await this.getWhirlpool();
-//     const tickArrayAddress = TickArrayEntity.deriveAddress(
-//       this.address,
-//       tickArrayStart,
-//       this.dal.programId
-//     );
-
-//     const tickArray = await this.dal.getTickArray(tickArrayAddress);
-//     invariant(!!tickArray, "OrcaWhirlpool - tickArray does not exist");
-//     return tickArray;
-//   }
-// }
--- src/pool/public/types.ts
+++ src/pool/public/types.ts
@@ -1,4 +1,3 @@
-import { TransactionPayload } from "@orca-so/whirlpool-client-sdk/dist/utils/transactions/transactions-builder";
 import { u64 } from "@solana/spl-token";
 import { Provider } from "@project-serum/anchor";
 import { PublicKey } from "@solana/web3.js";
@@ -79,51 +78,3 @@ export type SwapTransactionParam = {
 };
 
 export type SwapTransaction = TransactionExecutable;
-
-// export interface OrcaWhirlpool {
-//   getInitPoolTransaction: (initialSqrtPrice: any) => Promise<any>;
-
-//   getOpenPositionQuote: (
-//     tokenMint: PublicKey,
-//     tokenAmount: u64,
-//     tickLowerIndex: number,
-//     tickUpperIndex: number,
-//     slippageTolerence?: Percentage
-//   ) => Promise<{ maxTokenA: u64; maxTokenB: u64; liquidity: u64 }>;
-
-// create lp position
-// getOpenPositionTransaction: (
-//   owner: Owner,
-//   tokenAccountA: any,
-//   tokenAccountB: any,
-//   token: any,
-//   tokenAmount: any,
-//   tickLowerIndex: number,
-//   tickUpperIndex: number,
-//   slippageTolerence?: Percentage
-// ) => Promise<any>;
-
-// getSwapQuote: (
-//   swapAmount: SwapAmount<A, B>,
-//   slippageTolerence?: Percentage
-// ) => Promise<SwapQuote<A, B>>;
-
-// // TODO this needs to do some precalculation on sdk side to pass in tick array
-// // accounts that need to be traversed by contract
-// getSwapTransaction: (
-//   owner: Owner,
-//   tokenAccountA: any,
-//   tokenAccountB: any,
-//   amount: any,
-//   slippageTolerence?: Percentage
-// ) => Promise<any>;
-
-// loadTickArray: (tickIndex: number) => Promise<TickArrayAccount>;
-
-// // return distribution of liquidity
-// // required to visualize liquidity in UI
-// getLiquidityDistribution: () => Promise<any>;
-
-// // return the suggested price range
-// getSuggestedPriceRange: (conservative: boolean) => Promise<any>;
-// }
