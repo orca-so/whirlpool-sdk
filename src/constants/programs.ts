f2c70ee74d202d3ea4c8b9f397ef20c7ad0089f0
0xscuba <88916581+0xscuba@users.noreply.github.com>
2022-01-25 13:37:22 -0800

    devnet program id

--- src/constants/programs.ts
+++ src/constants/programs.ts
@@ -1,25 +1,23 @@
 import { PublicKey } from "@solana/web3.js";
 import { OrcaNetwork } from "./public/network";
 
-// TODO - update once we have actual contract addresses
 export function getWhirlpoolsConfig(network: OrcaNetwork): PublicKey {
   switch (network) {
     case OrcaNetwork.MAINNET:
-      return PublicKey.default;
+      throw new Error("TODO");
     case OrcaNetwork.DEVNET:
-      return PublicKey.default;
+      throw new Error("TODO");
     default:
       throw new Error(`type ${network} is an Unknown network`);
   }
 }
 
-// TODO - update once we have actual contract addresses
 export function getWhirlpoolProgramId(network: OrcaNetwork): PublicKey {
   switch (network) {
     case OrcaNetwork.MAINNET:
-      return PublicKey.default;
+      throw new Error("TODO");
     case OrcaNetwork.DEVNET:
-      return PublicKey.default;
+      return new PublicKey("BJCiu66eFG6zrMxCmP3AUVM7hk88egY43LR6eD9WkSXb");
     default:
       throw new Error(`type ${network} is an Unknown network`);
   }
