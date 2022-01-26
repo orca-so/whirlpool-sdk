d3c5cdac39ac3a9f251f43d330ca3633dbc54631
0xscuba <88916581+0xscuba@users.noreply.github.com>
2022-01-26 12:51:52 -0800

    default connections

--- src/client.ts
+++ src/client.ts
@@ -3,7 +3,7 @@ import { PositionData, WhirlpoolData } from "@orca-so/whirlpool-client-sdk/dist/
 import { Connection, PublicKey } from "@solana/web3.js";
 import { OrcaNetwork } from ".";
 import { OrcaAdmin } from "./admin/orca-admin";
-import { defaultNetwork } from "./constants/defaults";
+import { defaultNetwork, getDefaultConnection } from "./constants/defaults";
 import { getWhirlpoolProgramId, getWhirlpoolsConfig } from "./constants/programs";
 import { OrcaDAL } from "./dal/orca-dal";
 import { OrcaWhirlpool } from "./pool/orca-whirlpool";
@@ -11,6 +11,7 @@ import { OrcaPosition } from "./position/orca-position";
 
 export type OrcaWhirlpoolClientConfig = {
   network?: OrcaNetwork;
+  connection?: Connection;
   whirlpoolConfig?: PublicKey;
   programId?: PublicKey;
 };
@@ -21,8 +22,9 @@ export class OrcaWhirlpoolClient {
   public readonly admin: OrcaAdmin;
   public readonly dal: OrcaDAL;
 
-  constructor(connection: Connection, config?: OrcaWhirlpoolClientConfig) {
+  constructor(config?: OrcaWhirlpoolClientConfig) {
     const network = config?.network || defaultNetwork;
+    const connection = config?.connection || getDefaultConnection(network);
     const whirlpoolsConfig = config?.whirlpoolConfig || getWhirlpoolsConfig(network);
     const programId = config?.programId || getWhirlpoolProgramId(network);
 
--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,8 +1,18 @@
-import { Commitment } from "@solana/web3.js";
+import { Connection } from "@solana/web3.js";
 import { Percentage } from "../utils/public/percentage";
 import { OrcaNetwork } from "./public/network";
 
 export const defaultSlippagePercentage = Percentage.fromFraction(1, 1000); // 0.1%
 
 export const defaultNetwork: OrcaNetwork = OrcaNetwork.MAINNET;
-export const defaultCommitment: Commitment = "recent";
+
+export function getDefaultConnection(network: OrcaNetwork): Connection {
+  switch (network) {
+    case OrcaNetwork.MAINNET:
+      return new Connection("https://api.mainnet-beta.solana.com", "recent");
+    case OrcaNetwork.DEVNET:
+      return new Connection("https://api.devnet.solana.com", "recent");
+    default:
+      throw new Error(`type ${network} is an Unknown network`);
+  }
+}
