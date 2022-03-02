dfa53e580403b613d69d5a06ea6f506db6f08d1e
scuba <88916581+0xscuba@users.noreply.github.com>
2022-03-01 21:58:47 -0800

    get nearest valid tick index (#51)
    * getNearestValidTickIndex
    
    * more functions
    
    * bug fix

---
+++ src/utils/public/tick.ts
@@ -0,0 +1,18 @@
+import { sqrtPriceX64ToTickIndex, TickSpacing, toX64 } from "@orca-so/whirlpool-client-sdk";
+import Decimal from "decimal.js";
+import { TickUtil } from "../whirlpool/tick-util";
+
+export function getNearestValidTickIndex(price: Decimal, stable = false) {
+  const tickSpacing = stable ? TickSpacing.Stable : TickSpacing.Standard;
+  return TickUtil.toValid(sqrtPriceX64ToTickIndex(toX64(price.sqrt())), tickSpacing);
+}
+
+export function getNextValidTickIndex(tickIndex: number, stable = false) {
+  const tickSpacing = stable ? TickSpacing.Stable : TickSpacing.Standard;
+  return tickIndex + tickSpacing;
+}
+
+export function getPrevValidTickIndex(tickIndex: number, stable = false) {
+  const tickSpacing = stable ? TickSpacing.Stable : TickSpacing.Standard;
+  return tickIndex - tickSpacing;
+}
