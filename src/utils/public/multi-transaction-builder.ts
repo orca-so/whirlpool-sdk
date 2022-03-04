4f7d131e4efcd78f2e67a21ac5ad2ebb141a90b0
Yutaro Mori <yutaro@orca.so>
2022-03-04 16:18:38 +0800

    Bump sdk version

--- src/utils/public/multi-transaction-builder.ts
+++ src/utils/public/multi-transaction-builder.ts
@@ -20,7 +20,7 @@ export class MultiTransactionBuilder {
         return { tx: transaction, signers };
       })
     );
-    return await this.provider.sendAll(txRequest, { commitment: "singleGossip" });
+    return this.provider.sendAll(txRequest, { commitment: "singleGossip" });
   }
 
   public addTxBuilder(txBuilder: TransactionBuilder): MultiTransactionBuilder {
