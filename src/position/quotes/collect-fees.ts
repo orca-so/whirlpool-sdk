95ad0a2caaefb9c95be1fad1ae68e9cbe710ba44
tmoc <michael@tmoclabs.com>
2022-03-18 03:11:59 -0600

    Handling tick underflow cases that occur when global growth accumulator overflows (#74)

--- src/position/quotes/collect-fees.ts
+++ src/position/quotes/collect-fees.ts
@@ -1,6 +1,6 @@
 import { WhirlpoolData, PositionData, TickData } from "@orca-so/whirlpool-client-sdk";
 import { BN } from "@project-serum/anchor";
-import { subUnderflowU128, U128, ZERO } from "../../utils/web3/math-utils";
+import { subUnderflowU128 } from "../../utils/web3/math-utils";
 import { CollectFeesQuote } from "../public";
 
 export type InternalGetCollectFeesQuoteParam = {
@@ -44,8 +44,8 @@ export function getCollectFeesQuoteInternal(
   let feeGrowthBelowBX64: BN | null = null;
 
   if (tickCurrentIndex < tickLowerIndex) {
-    feeGrowthBelowAX64 = feeGrowthGlobalAX64.sub(tickLowerFeeGrowthOutsideAX64);
-    feeGrowthBelowBX64 = feeGrowthGlobalBX64.sub(tickLowerFeeGrowthOutsideBX64);
+    feeGrowthBelowAX64 = subUnderflowU128(feeGrowthGlobalAX64, tickLowerFeeGrowthOutsideAX64);
+    feeGrowthBelowBX64 = subUnderflowU128(feeGrowthGlobalBX64, tickLowerFeeGrowthOutsideBX64);
   } else {
     feeGrowthBelowAX64 = tickLowerFeeGrowthOutsideAX64;
     feeGrowthBelowBX64 = tickLowerFeeGrowthOutsideBX64;
@@ -58,16 +58,16 @@ export function getCollectFeesQuoteInternal(
     feeGrowthAboveAX64 = tickUpperFeeGrowthOutsideAX64;
     feeGrowthAboveBX64 = tickUpperFeeGrowthOutsideBX64;
   } else {
-    feeGrowthAboveAX64 = feeGrowthGlobalAX64.sub(tickUpperFeeGrowthOutsideAX64);
-    feeGrowthAboveBX64 = feeGrowthGlobalBX64.sub(tickUpperFeeGrowthOutsideBX64);
+    feeGrowthAboveAX64 = subUnderflowU128(feeGrowthGlobalAX64, tickUpperFeeGrowthOutsideAX64);
+    feeGrowthAboveBX64 = subUnderflowU128(feeGrowthGlobalBX64, tickUpperFeeGrowthOutsideBX64);
   }
 
   const feeGrowthInsideAX64 = subUnderflowU128(
-    feeGrowthGlobalAX64.sub(feeGrowthBelowAX64),
+    subUnderflowU128(feeGrowthGlobalAX64, feeGrowthBelowAX64),
     feeGrowthAboveAX64
   );
   const feeGrowthInsideBX64 = subUnderflowU128(
-    feeGrowthGlobalBX64.sub(feeGrowthBelowBX64),
+    subUnderflowU128(feeGrowthGlobalBX64, feeGrowthBelowBX64),
     feeGrowthAboveBX64
   );
 
